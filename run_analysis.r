abf5809a9d872d23547eb18e531c36fbb4410c1f
nichga <gareth.nicholson@aberdeen-asset.com>
2015-10-22 13:55:31 +0800

    Create run_analysis.r

---
+++ run_analysis.r
@@ -0,0 +1,47 @@
+#setwd("~/Coursea/Getting and Cleaning Data/")
+
+TrainingData = read.csv("UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE)
+TrainingData[,562] = read.csv("UCI HAR Dataset/train/Y_train.txt", sep="", header=FALSE)
+TrainingData[,563] = read.csv("UCI HAR Dataset/train/subject_train.txt", sep="", header=FALSE)
+
+testingData = read.csv("UCI HAR Dataset/test/X_test.txt", sep="", header=FALSE)
+testingData[,562] = read.csv("UCI HAR Dataset/test/Y_test.txt", sep="", header=FALSE)
+testingData[,563] = read.csv("UCI HAR Dataset/test/subject_test.txt", sep="", header=FALSE)
+
+activityLabels = read.csv("UCI HAR Dataset/activity_labels.txt", sep="", header=FALSE)
+
+# Read features and make the feature names better suited for R with some substitutions
+features = read.csv("UCI HAR Dataset/features.txt", sep="", header=FALSE)
+features[,2] = gsub('-mean', 'Mean', features[,2])
+features[,2] = gsub('-std', 'Std', features[,2])
+features[,2] = gsub('[-()]', '', features[,2])
+
+# Merge TrainingData and test sets together
+MergeData = rbind(TrainingData, testingData)
+
+# Get only the data on mean and std. dev.
+colsWeWant <- grep(".*Mean.*|.*Std.*", features[,2])
+# First reduce the features table to what we want
+features <- features[colsWeWant,]
+# Now add the last two columns (subject and activity)
+colsWeWant <- c(colsWeWant, 562, 563)
+# And remove the unwanted columns from MergeData
+MergeData <- MergeData[,colsWeWant]
+# Add the column names (features) to MergeData
+colnames(MergeData) <- c(features$V2, "Activity", "Subject")
+colnames(MergeData) <- tolower(colnames(MergeData))
+
+currentActivity = 1
+for (currentActivityLabel in activityLabels$V2) {
+        MergeData$activity <- gsub(currentActivity, currentActivityLabel, MergeData$activity)
+        currentActivity <- currentActivity + 1
+}
+
+MergeData$activity <- as.factor(MergeData$activity)
+MergeData$subject <- as.factor(MergeData$subject)
+
+tidy = aggregate(MergeData, by=list(activity = MergeData$activity, subject=MergeData$subject), mean)
+# Remove the subject and activity column, since a mean of those has no use
+tidy[,90] = NULL
+tidy[,89] = NULL
+write.table(tidy, "tidy.txt", sep="\t")
